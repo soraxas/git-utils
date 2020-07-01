b1d76e6743c8b5e88fc8b3fbd0504c7fe6133d7a
Tin Lai <oscar@tinyiu.com>
2020-07-02 00:30:26 +1000

    add more intelligent handling of git branch switching
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- completions/git-wip.fish
+++ completions/git-wip.fish
@@ -1,1 +1,6 @@
 complete -c git -x -n '__fish_git_using_command wip' -a '(__fish_git_branches)' -d 'BRANCH to switch to after action'
+
+complete -c git -f -n '__fish_git_using_command wip' -s c -l create -d 'enforce to create a wip commit'
+complete -c git -f -n '__fish_git_using_command wip' -s r -l revert -d 'enforce to revert a wip commit'
+
+complete -c git -f -n '__fish_git_using_command wip' -l version -d 'display version number'
